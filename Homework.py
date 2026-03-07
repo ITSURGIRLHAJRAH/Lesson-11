f2c2a1bd7219128c3fe6841753a525eaeed23287
Hajrah Javaid <saadi.hajrajavaid@gmail.com>
2026-03-07 13:33:12 +0500

    Add files via upload

---
+++ Homework.py
@@ -0,0 +1,8 @@
+r = int(input("Enter number of rows: "))
+for i in range(1, r+1):
+    for j in range(1, r+1):
+        if (j <= r - 1):
+            print(" ", end="  ")
+        else:
+            print("$", end="  ")
+    print()
